32eb20b2aeba63f24a3fcbaf392f0a518e21264b
Cuasicoromoto <cuasicoromoto@gmail.com>
2026-03-19 18:44:22 -0400

    Update cache version to v1.18 in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'coromoto-cache-v1.17'; // Recuerda subir este número
+const CACHE_NAME = 'coromoto-cache-v1.18'; // Recuerda subir este número
 const urlsToCache = [
     './',
     './index.html',
