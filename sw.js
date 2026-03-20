5bfd8df43b1fa20cd4566d2121785acee1394859
Cuasicoromoto <cuasicoromoto@gmail.com>
2026-03-19 21:40:51 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'coromoto-marzo-2026-v0.015'; // Recuerda subir este número
+const CACHE_NAME = 'coromoto-marzo-2026-v0.016'; // Recuerda subir este número
 const urlsToCache = [
     './',
     './index.html',
